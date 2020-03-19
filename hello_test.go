3ca75c2f57a07a14b10ca230d7472c8225e9750f
Max Beizer <max.beizer@gmail.com>
2020-03-19 10:26:03 -0500

    Wrap up hello

--- hello_test.go
+++ hello_test.go
@@ -9,15 +9,30 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say Hello world when no string passed in", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("amigo", "Spanish")
+		want := "Hola, amigo"
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("mon amis", "French")
+		want := "Bonjour, mon amis"
 		assertCorrectMessage(t, got, want)
 
 	})
