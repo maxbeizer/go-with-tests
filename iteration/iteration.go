dd7e7b0bd9da97a1e5072651261b3f1d7338f493
Max Beizer <max.beizer@gmail.com>
2020-03-19 12:46:14 -0500

    Add arrays/slices

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -7,3 +7,38 @@ func Repeat(character string, count int) string {
 	}
 	return repeated
 }
+
+func Sum(nums []int) int {
+	sum := 0
+
+	for _, number := range nums {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
