47d20430af2c013d084666643880f6646e1d268a
Max Beizer <max.beizer@gmail.com>
2020-03-28 13:10:12 -0500

    Add some of the maps chapter
    Up to
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#write-the-test-first-4

---
+++ maps/maps.go
@@ -0,0 +1,37 @@
+package maps
+
+type Dictionary map[string]string
+type DictionaryErr string
+
+const (
+	ErrorNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrorWordExists = DictionaryErr("cannot add word that already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrorNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrorNotFound:
+		d[word] = definition
+		return nil
+	case nil:
+		return ErrorWordExists
+	default:
+		return err
+	}
+}
